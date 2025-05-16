0557b178dade45ee826f0fb8695e3fc73d106207
Rony Villanueva <ronyvillame@gmail.com>
2025-05-16 17:35:30 -0500

    Gradle config and dependencies

---
+++ src/test/java/pe/com/hacom/oms/OmsApplicationTests.java
@@ -0,0 +1,12 @@
+package pe.com.hacom.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
