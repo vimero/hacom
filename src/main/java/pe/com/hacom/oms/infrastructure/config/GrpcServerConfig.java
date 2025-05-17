7bf631da702bb5b747d148b93e1c878f89f3bb83
Rony Villanueva <ronyvillame@gmail.com>
2025-05-17 08:03:23 -0500

    Akka classic actor

--- src/main/java/pe/com/hacom/oms/infrastructure/config/GrpcServerConfig.java
+++ src/main/java/pe/com/hacom/oms/infrastructure/config/GrpcServerConfig.java
@@ -1,8 +1,11 @@
 package pe.com.hacom.oms.infrastructure.config;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +15,11 @@ import pe.com.hacom.oms.application.port.in.CreateOrderUseCase;
 import pe.com.hacom.oms.grpc.OrderRequest;
 import pe.com.hacom.oms.grpc.OrderResponse;
 import pe.com.hacom.oms.grpc.OrderServiceGrpc;
+import pe.com.hacom.oms.infrastructure.actor.OrderProcessorActor;
 
 import java.io.IOException;
 
+@Slf4j
 @Configuration
 public class GrpcServerConfig {
 
@@ -22,32 +27,33 @@ public class GrpcServerConfig {
     private int grcpPort;
 
     @Bean
-    public Server grpcServer(CreateOrderUseCase createOrderUseCase) throws IOException {
+    public ActorSystem actorSystem() {
+        return ActorSystem.create("order-system");
+    }
+
+    @Bean
+    public ActorRef orderProcessorActor(ActorSystem system, CreateOrderUseCase createOrderUseCase) {
+        return system.actorOf(OrderProcessorActor.props(createOrderUseCase), "orderProcessor");
+    }
+
+    @Bean
+    public Server grpcServer(ActorRef orderProcessorActor) throws IOException {
         Server server = ServerBuilder.forPort(grcpPort)
                 .addService(new OrderServiceGrpc.OrderServiceImplBase() {
-
                     @Override
                     public void insertOrder(OrderRequest request, StreamObserver<OrderResponse> responseObserver) {
-                        var items = request.getItemsList().stream()
-                                .map(i -> new OrderItem(i.getItemId(), i.getQuantity()))
-                                .toList();
-
-                        var order = new Order(
+                        log.info("Recibida solicitud de orden gRPC: {}", request);
+                        Order order = new Order(
                                 request.getOrderId(),
                                 request.getCustomerId(),
                                 request.getPhoneNumber(),
-                                items
+                                request.getItemsList().stream()
+                                        .map(i -> new OrderItem(i.getItemId(), i.getQuantity()))
+                                        .toList()
                         );
 
-                        createOrderUseCase.createOrder(order);
-
-                        OrderResponse response = OrderResponse.newBuilder()
-                                .setOrderId(order.getOrderId())
-                                .setStatus("RECEIVED")
-                                .build();
-
-                        responseObserver.onNext(response);
-                        responseObserver.onCompleted();
+                        OrderProcessorActor.ProcessOrderMessage message = new OrderProcessorActor.ProcessOrderMessage(order, responseObserver);
+                        orderProcessorActor.tell(message, ActorRef.noSender());
                     }
                 })
                 .build();
